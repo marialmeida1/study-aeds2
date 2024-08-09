078c0a82d84fd8ed95f5818648c21c6038af7cb4
Mariana <marianaalmeidafga@gmail.com>
2024-08-09 10:35:04 -0300

    feat: fazendo exercícios de nivelamento

---
+++ nivelamento/exercicios/OnePesquisa.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+class OnePesquisa {
+
+   public boolean sequencial(int array[], int x){
+
+      for(int i=0; i<array.length; i++){
+         if(array[i] == x){
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   public void print_array(int array[]){
+      for (int i = 0; i < array.length; i++) {
+         MyIO.println(i + "º: " + array[i]);
+      }
+   }
+
+   public static void main(String[] args) {
+      
+      OnePesquisa pesquisa = new OnePesquisa();
+
+
+      int[] array = new int[10];
+      Random random = new Random();
+      for (int i = 0; i < array.length; i++) {
+         array[i] = random.nextInt(100);
+      }
+      
+      boolean resposta =  pesquisa.sequencial(array, 10);
+      pesquisa.print_array(array);
+      MyIO.println("A reposta e " + resposta);
+   }
+}
---
+++ nivelamento/exercicios/TwoPesquisaBina.java
@@ -0,0 +1,43 @@
+public class TwoPesquisaBina {
+
+    public boolean isCrescente(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) { // Se a posição atual for menor que a posição anterior, não está em ordem crescente
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int binaria(int[] array, int x) {
+        int inicio = 0;
+        int fim = array.length - 1;
+
+        while (inicio <= fim) {
+            int meio = inicio + (fim - inicio) / 2;
+
+            if (array[meio] == x) {
+                return meio;
+            }
+
+            if (array[meio] > x) {
+                fim = meio - 1; 
+            } else {
+                inicio = meio + 1; 
+            }
+        }
+
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        TwoPesquisaBina pesquisa = new TwoPesquisaBina();
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        boolean resposta = pesquisa.isCrescente(array);
+        int posicao = pesquisa.binaria(array, 5); // Alterado para procurar um valor presente no array
+
+        MyIO.println("Array está crescente? " + resposta);
+        MyIO.println("Posição do valor 5: " + posicao);
+    }
+}
