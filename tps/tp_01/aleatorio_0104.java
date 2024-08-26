eb2360dd77f9e897b9e1ea2fb51bddcc1593ef7c
Mariana <marianaalmeidafga@gmail.com>
2024-08-26 18:32:55 -0300

    feat: iniciando questão 4

--- tps/tp_01/aleatorio_0104.java
+++ tps/tp_01/aleatorio_0104.java
@@ -3,9 +3,42 @@ import java.util.Random;
 public class aleatorio_0104 {
     
 
-    public static void main(String[] args) {
+    public void replace(String str, char letter1, char letter2){
+
+        String strAux = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == letter1){
+                strAux += letter2;
+            } else {
+                strAux += str.charAt(i);
+            }
+        }
+        System.out.println(strAux);
+
+    }
+
+    public void generate(String str) {
         Random gerador = new Random();
         gerador.setSeed(4);
-        System.out.println((char)('a' + Math.abs(gerador.nextInt()) % 26));
+
+        char letter1 = (char)('a' + Math.abs(gerador.nextInt()) % 26);
+        char letter2 = (char)('a' + Math.abs(gerador.nextInt()) % 26);
+
+        replace(str, letter1, letter2);
+    }
+
+    public static void main(String[] args) {
+       aleatorio_0104 fnc = new aleatorio_0104();
+
+        String str = "";
+
+        while (true) {
+            str = MyIO.readLine("");
+            if (str.equals("FIM")) {
+                break;
+            }
+            fnc.generate(str);
+        }
     }
 }
